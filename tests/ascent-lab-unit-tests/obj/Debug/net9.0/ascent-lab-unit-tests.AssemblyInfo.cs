015b1919a7b7f8d49e7c53df223e95261600df17
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2025-03-13 06:47:52 -0300

    fix: remove certs https

--- tests/ascent-lab-unit-tests/obj/Debug/net9.0/ascent-lab-unit-tests.AssemblyInfo.cs
+++ tests/ascent-lab-unit-tests/obj/Debug/net9.0/ascent-lab-unit-tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e61ba9fa92b24f256657bc96487ad0be79cdc3b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533d9436c6ad79e0aaf2c523232df677ab08f4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
