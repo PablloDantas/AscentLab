b337b00957bd12b562267f8b871c2414279400e8
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2025-03-13 08:06:36 -0300

    feature: add render files

--- src/ascent-lab-web/obj/Debug/net9.0/ascent-lab-web.AssemblyInfo.cs
+++ src/ascent-lab-web/obj/Debug/net9.0/ascent-lab-web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ascent-lab-web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b323477c20b0b3ea110507d92f58ab69ff8f39f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0df7c8fe8ad34a8ff0c14e9068e3e719957150c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ascent-lab-web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ascent-lab-web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
