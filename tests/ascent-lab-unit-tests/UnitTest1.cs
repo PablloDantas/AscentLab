14bb812103c45291bbbf3accb88085f731fe3b4b
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2025-03-13 02:36:15 -0300

    feat: configuração inicial do projeto

---
+++ tests/ascent-lab-unit-tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace ascent_lab_unit_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        bool expected = true;
+
+        // Act
+        bool actual = true;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
+ 
