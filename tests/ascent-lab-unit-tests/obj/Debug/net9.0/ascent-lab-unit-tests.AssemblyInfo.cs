e9c4cdbb0d3ab070f5d3a803d99ab3445cfe493e
Pabllo Dantas <pabllo@ascentprojetos.com.br>
2025-03-13 09:16:56 -0300

    fix: docker files

--- tests/ascent-lab-unit-tests/obj/Debug/net9.0/ascent-lab-unit-tests.AssemblyInfo.cs
+++ tests/ascent-lab-unit-tests/obj/Debug/net9.0/ascent-lab-unit-tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9229c63f20484b1cd7c16118f56bae0fa2a5404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0daa6b8582238c3b1314601ec17f9ce90aa968")]
 [assembly: System.Reflection.AssemblyProductAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ascent-lab-unit-tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
